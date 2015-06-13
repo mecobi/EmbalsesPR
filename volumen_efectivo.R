bc1b3b082bf92075d04d0bca543abc3136668d0e
mecobi <mecobi@gmail.com>
2015-06-13 18:49:18 -0400

    Delete volumen_efectivo.R

--- volumen_efectivo.R
+++
@@ -1,7 +0,0 @@
-datos.embalses <- read.csv("embalses.csv",
-                           header=TRUE,
-                           comment.char="#")
-
-attach(datos.embalses)
-
-altura.efectiva <- desborde - control 
